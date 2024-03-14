897da98923d372bbfdd2fb2733bede0a90db253a
Nick Sinclair <nicksinclair@xtra.co.nz>
2024-03-14 18:08:04 +1300

    Use more primitive zod checker

--- src/openapi/schema.ts
+++ src/openapi/schema.ts
@@ -4,13 +4,28 @@ import { ParseFn } from "../route/parser";
 import { Parser } from "../route/parser/types";
 import { Debug } from "../debug";
 
+// Need to do this lame check because `instanceof` breaks once compiled
+// (for some reason)
+const isZod = (schema: unknown): schema is ZodSchema => {
+  return (
+    schema != null &&
+    typeof schema === "object" &&
+    "_def" in schema &&
+    schema._def != null &&
+    typeof schema._def === "object" &&
+    "typeName" in schema._def &&
+    typeof schema._def.typeName === "string" &&
+    schema._def.typeName.startsWith("Zod")
+  );
+};
+
 export const getJsonSchema = ({
   schema,
 }: {
   parser: ParseFn<unknown>;
   schema: Parser;
 }) => {
-  if (schema instanceof ZodSchema) {
+  if (isZod(schema)) {
     Debug.openapi("Is zod schema.");
     return zodToJsonSchema(schema);
   }
