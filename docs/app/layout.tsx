8d7ccf195f0ae42f98da1bb439a4e6f12022c3bc
Nick Sinclair <nicksinclair@xtra.co.nz>
2024-05-13 21:08:31 +1200

    Add route, composition docs

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,4 +1,5 @@
 import './global.css';
+import 'fumadocs-ui/twoslash.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
