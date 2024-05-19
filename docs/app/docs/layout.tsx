2c41dd55047de9e1d93a193fa4d4487af00c1886
Nick Sinclair <nicksinclair@xtra.co.nz>
2024-05-19 15:47:11 +1200

    Add beta warning

--- docs/app/docs/layout.tsx
+++ docs/app/docs/layout.tsx
@@ -4,8 +4,20 @@ import type { ReactNode } from "react";
 
 export default function RootDocsLayout({ children }: { children: ReactNode }) {
   return (
-    <DocsLayout tree={pageTree} nav={{ title: "Webroute" }}>
-      {children}
-    </DocsLayout>
+    <>
+      <section className="w-full py-2 px-4 flex items-center justify-center bg-blue-900 text-white text-xs gap-x-1 flex-wrap">
+        <span className="font-semibold">Webroute </span>
+        <span className="bg-white/50 px-2 rounded-full uppercase text-[9px] font-semibold">
+          beta
+        </span>
+        <span>
+          Relatively stable. Looking for more feedback before stabilising on
+          v1.0.
+        </span>
+      </section>
+      <DocsLayout tree={pageTree} nav={{ title: "Webroute" }}>
+        {children}
+      </DocsLayout>
+    </>
   );
 }
