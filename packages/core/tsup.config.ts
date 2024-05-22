1850f11b106d2eaf39750a3115af149945a98ddb
Nick Sinclair <nicksinclair@xtra.co.nz>
2024-05-22 19:10:36 +1200

    Add tree shaking

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -7,5 +7,6 @@ export default defineConfig({
   format: ["cjs", "esm"],
   outDir: "dist",
   sourcemap: false,
-  external: ["zod", "zod-to-json-schema"],
+  external: ["zod"],
+  treeshake: "recommended",
 });
