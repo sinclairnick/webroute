3366f6075ce6c81da1b3790b8f65404fe19132cb
Nick Sinclair <nicksinclair@xtra.co.nz>
2024-06-05 17:47:25 +1200

    Add infer, parse exports

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -1,3 +1,6 @@
+import { InferParser, Parser } from "./validation/types";
+import { getParseFn } from "./validation/validation";
+
 export { createFormatter } from "./formatter/formatter";
 export * from "./formatter/types";
 
@@ -8,3 +11,12 @@ export type * from "./def/schema-def";
 export type * from "./def/core";
 export type * from "./def/schema-def";
 
+export type Infer<T extends Parser> = InferParser<T>["out"];
+export type InferIn<T extends Parser> = InferParser<T>["in"];
+
+export const parse = <T extends Parser>(
+  parser: T,
+  value: unknown
+): Infer<T> => {
+  return getParseFn(parser)(value);
+};
