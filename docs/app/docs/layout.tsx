9055323578204798b043cf3e906292b3983af16d
Nick Sinclair <nicksinclair@xtra.co.nz>
2024-06-15 14:28:06 +1200

    Remove beta flag

--- docs/app/docs/layout.tsx
+++ docs/app/docs/layout.tsx
@@ -5,15 +5,6 @@ import type { ReactNode } from "react";
 export default function RootDocsLayout({ children }: { children: ReactNode }) {
   return (
     <>
-      <section className="w-full py-2 px-4 flex items-center justify-center bg-blue-900 text-white text-xs gap-x-1 flex-wrap">
-        <span className="font-semibold">Webroute </span>
-        <span className="bg-white/50 px-2 rounded-full uppercase text-[9px] font-semibold">
-          alpha
-        </span>
-        <span>
-          Core functionality is defined. Docs and API are being refined.
-        </span>
-      </section>
       <DocsLayout
         tree={pageTree}
         nav={{
